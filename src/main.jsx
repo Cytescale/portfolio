00b9f65d5d58c79434a787c5406e9c6633ad5282
cytescale <nikhil@cytescale.com>
2023-05-07 11:36:34 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { MouseContext, MouseContextProvider } from "./utills";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MouseContextProvider>
       <App />
+      <Analytics />
     </MouseContextProvider>
   </React.StrictMode>
 );
